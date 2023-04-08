f27599dad95854576366eb4fa7b6b50fa55d6db1
jose torres <andytorres25995@gmail.com>
2023-04-08 14:30:26 -0400

    all containers display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-$('#time').text(dayjs().format('dddd, MMMM d'))
+var time = $('#time').text(dayjs().format('dddd, MMMM d')); 
+var hour = ['9','10', '11', '12', '1', '2', '3', '4', '5'];
+$('p.9').text(hour[0] + 'am')
+$('p.10').text(hour[1] + 'am')
+$('p.11').text(hour[2] + 'am')
+$('p.12').text(hour[3] + 'am')
+$('p.1').text(hour[4] + 'pm')
+$('p.2').text(hour[5] + 'pm')
+$('p.3').text(hour[6] + 'pm')
+$('p.4').text(hour[7] + 'pm')
+$('p.5').text(hour[8] + 'pm')
